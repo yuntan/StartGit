8b287fe29221428296306cc19e52ed75a1cd4dc3
Yuuto Tokunaga <yuntan.sub1@gmail.com>
2014-04-27 20:04:24 +0900

    add osasshi image

--- myscript.js
+++ myscript.js
@@ -1,14 +1,23 @@
 +function(window) {
     var cj = createjs, stage, circle;
+    cj.DisplayObject.suppressCrossDomainErrors = true;
 
     function init() {
         stage = new cj.Stage('drawarea');
 
+        // 円を描く
         circle = new cj.Shape();
-        circle.graphics.beginFill('red').drawCircle(0, 0, 50);
+        circle.graphics.beginFill('red').drawCircle(0, 0, 50).endFill();
+        // 円の中心の座標を指定
         circle.x = 50; circle.y = 50;
-
         stage.addChild(circle);
+
+        // 画像を描画
+        bitmap = new cj.Bitmap("miku.jpg");
+        bitmap.x = 200; bitmap.y = 50;
+        bitmap.scaleX = 0.5; bitmap.scaleY = 0.5;
+        stage.addChild(bitmap);
+
         stage.update();
     }
 
